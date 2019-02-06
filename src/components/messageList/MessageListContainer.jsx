1138e100c6afaae716e3c31da9484ee36effee60
magpia13 <30365499+magpia13@users.noreply.github.com>
2019-02-06 01:47:59 +0100

    Update MessageListContainer.jsx

--- src/components/messageList/MessageListContainer.jsx
+++ src/components/messageList/MessageListContainer.jsx
@@ -5,26 +5,16 @@ import categoryAction from '../../store/actions/categoryAction';
 import messageAction from '../../store/actions/messageAction';
 
 class MessageListContainer extends Component {
-  constructor (props) {
-    super(props)
-    this.state = {
-
-
-    }
-  };
   componentDidMount(){
     this.props.getMessage();
     this.props.getCategory();
-
   }
 
   render() {
     const {message,category} = this.props;
-
-    console.log("sss",this.props.userById ? this.props.userById : null)
     return (
       <MessageList category={category} message={message} sender={this.props.userById.id} />
-      );
+     );
   }
 }
 
@@ -33,10 +23,9 @@ function mapStateToProps(state){
     message: state.message,
     userById:state.userById,
     category: state.category
-
-
   }
 };
+
 function mapDispatchToProps(dispatch) {
   return {
     getMessage: () => dispatch(messageAction.getMessage()),
